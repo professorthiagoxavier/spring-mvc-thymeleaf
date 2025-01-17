7c925889c398174ddf96cce0aec173343496864f
professorthiagoxavier <professorthiagoxavier>
2025-01-16 22:20:02 -0300

    interação

--- src/main/java/com/example/myappthymeleaf/controllers/EstrturaBasicaController.java
+++ src/main/java/com/example/myappthymeleaf/controllers/EstrturaBasicaController.java
@@ -1,8 +1,13 @@
 package com.example.myappthymeleaf.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import com.example.myappthymeleaf.model.Produto;
+
 
 @Controller
 public class EstrturaBasicaController {
@@ -10,6 +15,12 @@ public class EstrturaBasicaController {
     @GetMapping("/estrutura-basica")
     public String listSeedStarters(Model model) {
        
+    	List<Produto> lista = new ArrayList<Produto>();
+    	lista.add(new Produto("Smartphone", 1000.0));
+    	lista.add(new Produto("Notebook", 3000.0));
+    	
+    	
+    	model.addAttribute("produtos",lista);
     	//variável básica
     	model.addAttribute("mensagem", "Bem-vindo, aluno Fiap!");
     	return "index";
