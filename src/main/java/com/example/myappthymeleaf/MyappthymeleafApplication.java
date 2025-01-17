205240b51c8b651f876a4c4462d6658259dbbd5c
professorthiagoxavier <professorthiagoxavier>
2025-01-16 21:57:35 -0300

    estratura básica

---
+++ src/main/java/com/example/myappthymeleaf/MyappthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.example.myappthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyappthymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyappthymeleafApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/myappthymeleaf/controllers/EstrturaBasicaController.java
@@ -0,0 +1,17 @@
+package com.example.myappthymeleaf.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EstrturaBasicaController {
+
+    @GetMapping("/estrutura-basica")
+    public String listSeedStarters(Model model) {
+       
+    	//variável básica
+    	model.addAttribute("mensagem", "Bem-vindo, aluno Fiap!");
+    	return "index";
+    }
+}
