02051302996eb3ec9cf44a0eb97aac0b22a697c6
professorthiagoxavier <professorthiagoxavier>
2025-01-18 07:15:45 -0300

    anotações RequestParam

--- src/main/java/com/example/myappthymeleaf/controllers/AtributosController.java
+++ src/main/java/com/example/myappthymeleaf/controllers/AtributosController.java
@@ -5,11 +5,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.example.myappthymeleaf.model.Funcionario;
 
 @Controller
 public class AtributosController {
 
+	@GetMapping("/buscar")
+	@ResponseBody
+	public String buscar(@RequestParam("termo") String termo) {
+	    // Lógica para buscar com o termo fornecido
+		//System.out.println(termo);
+	    return "resultados " + termo;
+	}
+
+	
     @GetMapping("/formFuncionario")
     public String showForm(Model model) {
         model.addAttribute("funcionario", new Funcionario());
